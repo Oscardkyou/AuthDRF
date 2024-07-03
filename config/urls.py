e56982bb124a453e8e992318af731ef4d57cc856
Oscardk <dauren2050@yandex.ru>
2024-07-03 21:36:14 +0500

    fix code v2

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path("admin/", admin.site.urls),
-    path('', include('my_auth.urls'))
+    path('lorby/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('lorby/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("lorby/admin/", admin.site.urls),
+    path('lorby', include('my_auth.urls'))
 ]
